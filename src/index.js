496ff7b4243437580492ab009acab7dd45632337
Jacek Walasik <jacekwalasik89@gmail.com>
2019-02-18 22:15:09 +0100

    login fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import Firebase, { FirebaseContext} from './firebase'
 
 ReactDOM.render(
 	<FirebaseContext.Provider value={new Firebase()}>
-		<App/>
+	  <App />
 	</FirebaseContext.Provider>,
-document.getElementById('root'));
+	document.getElementById('root'),
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
